21dbe957f1064226a7a65fc791974950898e8a3b
Jillium <86424914+Jillium@users.noreply.github.com>
2022-02-01 14:47:24 -0700

    content added, footer not working

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 // import Project from "./components/Project";
 import Header from "./components/Header";
+import About from "./components/About";
 // import Footer from "./components/Footer";
+
 
 function App() {
   return (
     <div className="App">
       <Header />
-      {/* 
-      <Project />
-      <Footer /> */}
-    </div>
+      <About />
+      
+     
+      {/* <Footer /> */}
+    </div> 
   );
 }
 
--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 function Footer() {
-   
+   <div className="footer-div">
+       <footer className="footer">
+       <h2>This is a footer</h2>
+       </footer>
+   </div>
 }
 
 export default Footer;
