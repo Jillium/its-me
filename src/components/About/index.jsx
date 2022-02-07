5b017abd7a3ccf9526b7b79841f68516485417a8
Jillium <86424914+Jillium@users.noreply.github.com>
2022-02-06 18:47:04 -0700

    fixing a couple of styling

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -3,7 +3,7 @@ import profilePic from "../../assets/profile.JPG";
 
 function About() {
     return (
-        <div>
+        <div className='about-container'>
             <img src={profilePic} className='profile-img' alt="profile"></img>
        <p className='about'>Full stack web developer looking to utilize customer service and quality assurance background to create a balanced user experience while utilizing web applications. Currently a full stack flex web development student with the University of Utah coding bootcamp. Newly developed skills in Javascript, CSS, HTML, node.js, MySQL, React, MERN, and mongoDB. Focused on responsive web design and great user experience. Recently worked on a team of 4 to build a fan based music website that utilizes the MVC paradigm. Server side rendering with handlebars.js was also used, along with sequelize to manage the database. I am hoping to leverage my previous skills along with my new ones in a role where I can continue progressing. </p>
        </div>
---
+++ src/components/Portfolio/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Project from "../Project/index.jsx";
+
+function Portfolio() {
+
+
+    return (
+        <Project />
+    )
+}
+
+export default Portfolio;
